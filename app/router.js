6abc7f6a904839c85d12a89a942e3495759e15dd
Brandon Gordon <brandon-g32@hotmail.com>
2018-12-28 13:12:39 -0500

    Setting up routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,16 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('services');
+  this.route('contact');
+
+  /*This route will be found in the in the templates/posts/index and the "new" is in the /posts/new route.
+  * This is basically a nested route. 
+  */
+  this.route('posts', { path: '/posts' }, function () {
+    this.route('new');
+  });
 });
 
 export default Router;
