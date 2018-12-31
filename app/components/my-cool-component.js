bf851dd9c45555e797cd2e514039bfae7f48c119
Brandon Gordon <brandon-g32@hotmail.com>
2018-12-31 14:05:45 -0500

    - Finished simple component --> Pretty fun!

---
+++ app/components/my-cool-component.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  name: 'JS Value',
+  greetingSet: false,
+  actions: {
+    //name will be passed into here from the .hbs 
+    greetUser: function (name) {
+      alert("Greeting User " + name);
+      this.setProperties({
+        greetingSet: true
+      });
+    }
+  },
+  /* Here, we are going to wrap the component in this class. Defining .well class in the .css(.scss) file will
+  allow us to define it */
+  classNames: ['well'],
+  //when greetingSet is true (after clicking the button), it will add a class called .greeting-set
+  classNameBindings: ['greetingSet']
+});
